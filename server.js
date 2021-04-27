325755f7fd1c071045e94e1882510c3ee9499285
Chafik El idrissi <c.elidrissi@thirard.fr>
2021-04-27 22:01:06 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,48 @@
+// Déclarations des variables + Création de l'instance serveur 
+var express = require('express');
+var server = express();
+var hostname = '127.0.0.1';
+var port = 80;
+const fs = require('fs');
+require('console-stamp')(console, '[HH:MM:ss.l]');
+
+var json = {};
+
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+
+// Définition URL / renvoi vers l'index
+server.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+server.post('/result.html', function(req, res) {
+    var start = req.body.start; 
+    var end = req.body.end; 
+    var json = {};
+
+    console.log("Receive Post");
+    for (var i = start; i <= end; i++){
+        json[i] = {
+            nim : (i % 3 === 0),
+            buzz : (i % 5 === 0)
+        }
+    }
+    
+    let data = JSON.stringify(json, null, 2);
+    fs.writeFile('result.json', data, (err) => {
+        if (err) throw err;
+        console.log('Result written to JSON file');
+    });
+
+    res.sendFile( __dirname  + '/index.html');
+});
+
+server.get('/getResult', function(req, res){
+    var result = JSON.parse(fs.readFileSync('result.json', 'utf8'));
+    res.json(result);
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
